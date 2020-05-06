ab5f973d5a1ee09c9b120b529f8c37845e2533a3
typewriter <takuyaxp@gmail.com>
2020-05-06 17:06:09 +0900

    Add random view (for local development)

--- viewer/src/views/Random.vue
+++ viewer/src/views/Random.vue
@@ -2,18 +2,18 @@
   <div style="width: 100vw; height: 100vh;" class="uk-light uk-flex uk-flex-center uk-flex-middle uk-text-small">
     <div id="loading" v-if="loading">
       <div uk-spinner="ratio: 3"></div>
-      <br>読み込んでいます...
+      <br>読み込んでいます...<br><small>（ネットワーク回線の状態によっては時間が掛かる場合があります）</small>
     </div>
     <div v-if="!loading">
       <transition name="fade" mode="out-in" appear>
         <div id="even" key="even" v-if="even" style="width: 100vw; height: 100vh;" class="uk-background-contain" v-bind:style="{ backgroundImage: evenImage }"><!-- :style="{ backgroundImage: 'url(' + require('@/assets/imgs/monet/stacksofwheat-endofsummer.jpg') + ')' }"> -->
           <transition name="fade">
-            <div v-if="description" class="uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom">OVERLAY MESSAGE</div>
+            <div v-if="description" class="uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom-left uk-position-small uk-text-left" v-html="evenDescription"></div>
           </transition>
         </div>
         <div id="odd" key="odd" v-else style="width: 100vw; height: 100vh;" class="uk-background-contain" v-bind:style="{ backgroundImage: oddImage }"><!-- " :style="{ backgroundImage: 'url(' + require('@/assets/imgs/monet/waterlilies.jpg') + ')' }"> -->
           <transition name="fade">
-            <div v-if="description" class="uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom">OVERLAY MESSAGE</div>
+            <div v-if="description" class="uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom-left uk-position-small uk-text-left" v-html="oddDescription"></div>
           </transition>
         </div>
       </transition>
@@ -26,32 +26,68 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'Random',
   data: function() { return {
-    loading: true,     // 初期ロード
-    even: true,        // どちらを表示しているか
-    description: true, // 説明表示
-    evenImage: "",     // イメージアドレス
-    oddImage: ""       // イメージアドレス
+    server: "http://localhost:4567",
+    apiPath: "/random",  // APIエンドポイント
+    loading: true,       // 初期ロード
+    even: true,          // どちらを表示しているか
+    description: true,   // 説明表示
+    evenImage: "",       // イメージアドレス
+    evenDescription: "",
+    oddImage: "",        // イメージアドレス
+    oddDescription: "",
     // ロード中、切替時間などを可変とする予定である
   }},
   methods: {
     switch() {
       this.description = true
       this.even = !this.even
-      this.loaded()
+      this.fetchNextArtwork()
     },
-    hideDescription() {
-      this.description = false
+    initialLoad() {
+      fetch(this.server + this.apiPath + "?i=1")
+        .then(function(response) {
+          return response.json();
+        }).then(json => {
+          const imageUrl = this.server + json["image_url"]
+          const description = `${json["artist"]}<br><strong>${json["title"]}</strong><br><small>${json["date"]}<br>${json["medium"]}<br>${json["credit"]}</small><!-- ${json["description"]} -->`
+          this.evenImage = `url("${imageUrl}")`
+          this.evenDescription = description
+
+          // duplicate load
+          const img = new Image()
+          img.onload = () => {
+            this.loading = false
+            this.fetchNextArtwork()
+          }
+          img.src = imageUrl
+        })
     },
-    loaded() {
-      setTimeout(this.switch, 10000)
-      // setTimeout(this.hideDescription, 5000)
+    fetchNextArtwork() {
+      // fetch next artwork
+      fetch(this.server + this.apiPath)
+        .then(function(response) {
+          return response.json();
+        }).then(json => {
+          const imageUrl = this.server + json["image_url"]
+          const description = `${json["artist"]}<br><strong>${json["title"]}</strong><br><small>${json["date"]}<br>${json["medium"]}<br>${json["credit"]}</small><!-- ${json["description"]} -->`
+          if (this.even) {
+            this.oddImage = `url("${imageUrl}")`
+            this.oddDescription = description
+          } else {
+            this.evenImage = `url("${imageUrl}")`
+            this.evenDescription = description
+          }
+
+          // preload
+          const img = new Image()
+          img.src = imageUrl
+        })
+      setTimeout(() => this.description = false, 30000)
+      setTimeout(this.switch, 60000)
     }
   },
   mounted() {
-    this.evenImage = 'url("/stacksofwheat-endofsummer.jpg")'
-    this.oddImage = 'url("/waterlilies.jpg")'
-    this.loading = false
-    this.loaded()
+    this.initialLoad()
   }
 })
 </script>
