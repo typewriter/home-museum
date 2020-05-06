e0a237d915d3c1fe57d97cb19154198666644b8d
typewriter <takuyaxp@gmail.com>
2020-05-06 22:44:54 +0900

    Add OGP

--- viewer/vue.config.js
+++ viewer/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-        args[0].title = 'Home museum'
+        args[0].title = 'Home museum (β) - 画面いっぱいに美術作品を楽しもう'
         return args
       })
   }
