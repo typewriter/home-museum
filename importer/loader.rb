5a34da25365dad64241317cf7a68f26a7cba57f9
typewriter <takuyaxp@gmail.com>
2020-05-07 23:17:22 +0900

    Support MET load

--- importer/loader.rb
+++ importer/loader.rb
@@ -5,8 +5,6 @@ require "json"
 require "sqlite3"
 
 DATABASE_PATH = ENV['DATABASE_PATH'] || "./hm.db"
-IMPORT_PATH   = ENV['IMPORT_PATH'] || ARGV[0]
-
 db = SQLite3::Database.new DATABASE_PATH
 
 # create table
@@ -58,41 +56,74 @@ def aic_loader(path)
   records
 end
 
+def met_loader(path)
+  records = []
+  db = LevelDB::DB.new path
+  db.each { |id, v|
+    json = JSON.parse(v)
+
+    next if !json["isPublicDomain"]
+    next if !json["primaryImage"]
+
+    records << {
+      source: 'MET',
+      category: json["classification"],
+      style: json["department"],
+      title: json["title"],
+      artist: json["artistDisplayName"],
+      date: json["objectDate"],
+      medium: json["medium"],
+      origin: nil,
+      dimensions: json["dimensions"],
+      credit: json["creditLine"],
+      description: nil,
+      source_url: json["objectURL"],
+      image_url: json["primaryImage"],
+    }
+  }
+  records
+end
+
 # insert or update
-records = aic_loader(IMPORT_PATH)
+records = aic_loader(ARGV[0])
+records += met_loader(ARGV[1])
 
-records.each { |record|
-  # source_url単位
-  sql = <<-SQL
-    select 1
-      from images
-      where source_url = ?
-  SQL
+records.each_slice(1000) { |slice_records|
+  db.transaction {
+    slice_records.each { |record|
+      # source_url単位
+      sql = <<-SQL
+        select 1
+          from images
+          where source_url = ?
+      SQL
 
-  if db.execute(sql, record[:source_url]).length > 0
-    # FIXME: update not supported
-    puts "exists: #{record[:source_url]}"
-    next
-  end
+      if db.execute(sql, record[:source_url]).length > 0
+        # FIXME: update not supported
+        puts "exists: #{record[:source_url]}"
+        next
+      end
 
-  puts "insert: #{record[:source_url]}"
-  sql = <<-SQL
-    insert into images(source, category, style, title, artist, date, medium, origin, dimensions, credit, description, source_url, image_url)
-      values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  SQL
-  db.execute(sql,
-             record[:source],
-             record[:category],
-             record[:style],
-             record[:title],
-             record[:artist],
-             record[:date],
-             record[:medium],
-             record[:origin],
-             record[:dimensions],
-             record[:credit],
-             record[:description],
-             record[:source_url],
-             record[:image_url]
-            )
+      puts "insert: #{record[:source_url]}"
+      sql = <<-SQL
+        insert into images(source, category, style, title, artist, date, medium, origin, dimensions, credit, description, source_url, image_url)
+          values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      SQL
+      db.execute(sql,
+                 record[:source],
+                 record[:category],
+                 record[:style],
+                 record[:title],
+                 record[:artist],
+                 record[:date],
+                 record[:medium],
+                 record[:origin],
+                 record[:dimensions],
+                 record[:credit],
+                 record[:description],
+                 record[:source_url],
+                 record[:image_url]
+                )
+    }
+  }
 }
